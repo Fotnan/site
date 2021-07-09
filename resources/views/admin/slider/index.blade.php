e7dcd52bf86ba1aac30832ff9a8cecb7ad5a047d
Vadim <present@hot.ee>
2021-07-09 19:01:15 +0300

    Slider add

---
+++ resources/views/admin/slider/index.blade.php
@@ -0,0 +1,63 @@
+@extends('admin.admin_master')
+
+@section('admin')
+
+    <div class="py-12">
+        <div class="container">
+            <div class="row">
+                <h4>Home Slider</h4> <br>
+                <a href="{{route('add.slider')}}"><button class="btn btn-info">Add Slider</button></a>
+                <div class="col-md-12">
+                    <div class="card">
+
+
+
+
+                        <div class="card-header"> All Slider </div>
+
+
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">SL </th>
+                                <th scope="col">Slider Title</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Slider Image</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                             @php($i = 1)
+                            @foreach($sliders as $slider)
+                                <tr>
+                                    <th scope="row"> {{ $i++  }} </th>
+                                    <td> {{ $slider->title }} </td>
+                                    <td> {{ $slider->description }} </td>
+                                    <td> <img src="{{ asset($slider->image) }}" style="height:40px; width:70px;" > </td>
+
+                                    <td>
+                                        <a href="{{ url('slider/edit/'.$slider->id) }}" class="btn btn-info">Edit</a>
+                                        <a href="{{ url('slider/delete/'.$slider->id) }}" onclick="return confirm('Are you sure to delete')" class="btn btn-danger">Delete</a>
+                                    </td>
+
+
+                                </tr>
+                            @endforeach
+
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+
+
+
+    </div>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,11 +17,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $brands = DB::table('brands')->get();
+    return view('home', compact('brands'));
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('admin.index');
 })->name('dashboard');
 
+Route::get('/brand/all', [BrandController::class, 'AllBrand'])->name('all.brand');
+Route::post('/brand/add', [BrandController::class, 'StoreBrand'])->name('store.brand');
+Route::get('/brand/edit/{id}', [BrandController::class, 'Edit']);
+Route::post('/brand/update/{id}', [BrandController::class, 'Update']);
+Route::get('/brand/delete/{id}', [BrandController::class, 'Delete']);
+
 Route::get('/user/logout', [BrandController::class, 'Logout'])->name('user.logout');
+
+
+// Multi Image Route
+Route::get('/multi/image', [BrandController::class, 'Multpic'])->name('multi.image');
+Route::post('/multi/add', [BrandController::class, 'StoreImg'])->name('store.image');
+
+//Admin ALL Route
+Route::get('/home/slider', [HomeController::class, 'HomeSlider'])->name('home.slider');
+Route::get('/add/slider', [HomeController::class, 'AddSlider'])->name('add.slider');
+Route::post('/store/slider', [HomeController::class, 'StoreSlider'])->name('store.slider');
